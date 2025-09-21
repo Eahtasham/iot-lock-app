a9fc768098622c16c3a330b43934ae5107aac8da
Eahtasham <eahtashamummam@gmail.com>
2025-09-22 02:05:01 +0530

    adds custom colors theme and all like web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from 'react-native';
-
 import { HelloWave } from '@/components/hello-wave';
+import { Camera, Heart } from "lucide-react-native";
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold text-blue-600">Hello Expo + NativeWind 🚀</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      {/* <Text className="text-3xl font-bold text-blue-600">Hello Expo + NativeWind 🚀</Text> */}
+      <Camera color="black" size={32} />
+      <Heart color="red" size={32} />
       <HelloWave />
+      <Text variant="headlineMedium">Hello</Text>
+      <Button className='bg-secondary' mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { PaperProvider } from 'react-native-paper';
 import 'react-native-reanimated';
 import './globals.css';
 
@@ -14,6 +15,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    <PaperProvider>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -21,5 +23,6 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </PaperProvider>
   );
 }
